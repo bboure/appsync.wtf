037f19647e40ca70128024f2b412bc45f2644cfd
bboure <benoit.boure@gmail.com>
2024-01-04 17:33:09 +0100

    Fix Github link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -96,7 +96,7 @@ const config: Config = {
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/bboure/appsync.wtf',
           label: 'GitHub',
           position: 'right',
         },
